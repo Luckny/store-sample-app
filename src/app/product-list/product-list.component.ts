ee793e9a8b798991e41561a85a6a4e48e095c09f
Luckny <93001262+Luckny@users.noreply.github.com>
2022-07-07 09:23:30 -0400

    Create the product list

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent implements OnInit {
+  constructor() {}
+
+  public products: Product[] = products;
+
+  public share(): void {
+    window.alert('The product has been shared!');
+  }
+
+  ngOnInit(): void {}
+}
